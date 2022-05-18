233d86fc1497a6a2106edfb353aa3fa80fcf9740
FRACEtcg <eugene.opalihin@gmail.com>
2022-05-18 07:07:19 +0300

    added clear and showgrid buttons, click-hover coloring

--- script.js
+++ script.js
@@ -7,16 +7,38 @@ for (var i = 0; i < n * n; i++) {
 	x.classList.add('cell');
 	container.appendChild(x);
 }
+let ifborders = true;
+let show = document.getElementById('show');
+show.addEventListener('click', () => { // shows/hides borders of grid
+	if (ifborders) {
+		Array.from(document.querySelectorAll('.cell')).forEach((e) => {
+			e.style.border = 'none';
+		});
+		ifborders = false;
+	} else {
+		Array.from(document.querySelectorAll('.cell')).forEach((e) => {
+			e.style.border = 'solid 1px rgb(75, 61, 93)';
+		});
+		ifborders = true;
+	}
+})
+let clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+		Array.from(document.querySelectorAll('.cell')).forEach((e) => {
+			e.style.backgroundColor = 'black';
+		});
+})
 let cells = document.querySelectorAll('.cell');
 let reset = document.querySelector('.container');
 reset.addEventListener('mousedown', () => {toggle()});
-let addev = function(e)  { //passes to addeventlistener function, an element that changes colorbg
+let addev = function(e)  { //coloring function
 	e.target.style.backgroundColor = 'white';
 };
-function toggle() {
+function toggle() { // toggle between painting/moving around
 	toggler = !toggler;
 	if (toggler === true) {
 		Array.from(cells).forEach((element) => {
+			element.addEventListener('click', addev);
 			element.addEventListener('mouseover', addev);
 	})} else {
 		Array.from(cells).forEach((element) => {
